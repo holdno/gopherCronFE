75b54fb12370538c6e6b1514ed6aed079cf95300
林玮 (Jade Lin) <linw1995@icloud.com>
2021-12-28 18:36:26 +0800

    feat: 添加各项目任务数量统计饼图

---
+++ src/components/ProjectsPieChart.vue
@@ -0,0 +1,111 @@
+<template>
+  <div ref="box" class="tw-h-full tw-w-full"></div>
+</template>
+
+<script setup lang="ts">
+  import { onMounted, ref, PropType, computed, watchEffect, toRaw } from 'vue';
+  import * as echarts from 'echarts/core';
+  import { PieChart } from 'echarts/charts';
+  import {
+    TitleComponent,
+    TooltipComponent,
+    GridComponent,
+    DatasetComponent,
+    TransformComponent,
+  } from 'echarts/components';
+  import { LabelLayout, UniversalTransition } from 'echarts/features';
+  import { CanvasRenderer } from 'echarts/renderers';
+  import { Project } from '../request';
+
+  const props = defineProps({
+    projects: {
+      type: Object as PropType<Project[]>,
+      required: true,
+    },
+  });
+
+  const box = ref<HTMLElement>();
+  const options = computed(() => {
+    return {
+      // backgroundColor: '#2c343c',
+      title: {
+        text: '项目任务占比图',
+        left: 10,
+        top: 0,
+        textStyle: {
+          color: '#fff',
+        },
+      },
+
+      tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b} : {c} ({d}%)',
+        backgroundColor: 'rgba(0,0,0,1)',
+        textStyle: {
+          color: 'rgba(153, 246, 228, 1)',
+        },
+      },
+
+      series: [
+        {
+          name: '任务数',
+          type: 'pie',
+          radius: '55%',
+          center: ['50%', '50%'],
+          data: props.projects
+            .map((v) => ({
+              value: v.taskCount,
+              name: v.title,
+            }))
+            .sort((a, b) => a.value - b.value),
+          roseType: 'radius',
+          label: {
+            textStyle: {
+              color: 'rgba(255, 255, 255, 0.3)',
+            },
+          },
+          labelLine: {
+            lineStyle: {
+              color: 'rgba(255, 255, 255, 0.3)',
+            },
+            smooth: 0.2,
+            length: 10,
+            length2: 20,
+          },
+          itemStyle: {
+            color: 'rgba(153, 246, 228, 1)',
+          },
+
+          animationType: 'scale',
+          animationEasing: 'elasticOut',
+          animationDelay: function () {
+            return Math.random() * 200;
+          },
+        },
+      ],
+    };
+  });
+
+  const chart = ref<echarts.ECharts>();
+  onMounted(async () => {
+    if (box.value === undefined) throw new Error('box missing');
+    echarts.use([
+      TitleComponent,
+      TooltipComponent,
+      GridComponent,
+      DatasetComponent,
+      TransformComponent,
+      PieChart,
+      LabelLayout,
+      UniversalTransition,
+      CanvasRenderer,
+    ]);
+    chart.value = echarts.init(box.value);
+  });
+  watchEffect(() => {
+    if (chart.value && options.value) {
+      const raw = toRaw(chart.value);
+      raw.setOption(options.value);
+    }
+  });
+</script>
--- src/pages/SummaryPage.vue
+++ src/pages/SummaryPage.vue
@@ -1,16 +1,26 @@
 <template>
-  <div class="tw-w-1/2 tw-h-1/2">
-    <RecentLogCountChart :records="records" />
+  <div class="lg:tw-flex lg:tw-justify-around tw-w-full tw-h-full q-pa-sm">
+    <div class="tw-w-full tw-h-full">
+      <RecentLogCountChart :records="records" />
+    </div>
+    <div class="tw-w-full tw-h-full">
+      <ProjectsPieChart :projects="projects" />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
   import { computed, onMounted } from 'vue';
   import RecentLogCountChart from '../components/RecentLogCountChart.vue';
+  import ProjectsPieChart from '../components/ProjectsPieChart.vue';
   import { useStore } from '../store';
 
   const store = useStore();
   const records = computed(() => store.state.recentLogCountRecords);
+  const projects = computed(() => store.state.projects);
 
-  onMounted(async () => await store.dispatch('recentLog'));
+  onMounted(async () => {
+    await store.dispatch('recentLog');
+    await store.dispatch('fetchProjects');
+  });
 </script>
